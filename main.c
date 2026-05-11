a5e1936028edb8a1313b09302b662f103e32de21
異次元のA <165989579+450395Ayumu@users.noreply.github.com>
2026-05-11 14:18:17 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
-// 基礎プログラミングII 第3回 Scrummyプロジェクト
+// 基礎プログラミングII SchooMy 衝撃検知センサー
 
 #include <stdio.h>
 
 int main() {
-    printf("Hello zzzzzzzzzzzzz!\n");
+    int soundValue;
+    int threshold = 120;
+
+    while (1) {
+        soundValue = getSoundSensor();
+
+        if (soundValue > threshold) {
+            ledOn();
+            wait(500);
+            ledOff();
+        } else {
+            ledOff();
+        }
+    }
 
     return 0;
 }
